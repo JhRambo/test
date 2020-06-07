7a480704936f3cb9655cf4b5530360545c80c65b
zqw <237230999@qq.com>
2020-06-07 19:06:05 +0800

    060701

--- test.php
+++ test.php
@@ -1577,6 +1577,29 @@
 // echo $y.PHP_EOL;
 
 #105
-$str1 = 'abc';
-$str2 = 'def';
-echo "{$str1}{$str2}";
+// $str1 = 'abc';
+// $str2 = 'def';
+// echo "{$str1}{$str2}";
+
+#106 redis lua脚本
+// $lua = <<<EOF
+// local num = redis.call('GET', KEYS[1]);  
+
+// if not num then
+// 	return 0;
+// else
+// 	local res = num * ARGV[1]; 
+// 	redis.call('SET',KEYS[1], res); 
+// 	return res;
+// end
+
+// EOF;
+
+// $redis = new Redis();
+// $redis->connect('127.0.0.1', 6379);
+// $redis->auth(123);
+// $ret = $redis->eval($lua, array("lua:incrbymul", 2), 1);
+// echo $ret;
+
+#107 test git reset --soft --hard --mixed
+ 
